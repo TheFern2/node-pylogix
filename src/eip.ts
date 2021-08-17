f957fba5fa2a23b24e81c8630bc90acebb016e26
Fernando B <fernandobe+git@protonmail.com>
2021-08-17 14:55:20 -0500

    u

--- src/eip.ts
+++ src/eip.ts
@@ -1,4 +1,5 @@
 import { Connection } from './lgx_comm'
+import { Response } from './lgx_response';
 
 export class PLC {
   public IPAddress;
@@ -27,12 +28,58 @@ export class PLC {
 
   }
 
-  private _Read(tag_name:string, elements:number, data_type:number){
+  private _ReadTag(tag_name:string, elements:number, data_type:number){
     // Processes the read request
     this.Offset = 0;
     const conn = this.conn.connect()
     if(!conn?.connected){
-      
+      return new Response(tag_name, null, conn?.status)
     }
   }
+}
+
+function parse_tag_name(tag_name:string){
+  /**
+    Parse the tag name into it's base tag (remove array index and/or
+    bit) and get the array index if it exists
+
+    ex: MyTag.Name[42] returns:
+    MyTag.Name[42], MyTag.Name, 42
+  **/
+    const bit_end_pattern = /\.\d+$/ // this finds tag names with bits BaseBITS.4
+    const array_pattern = /\[([\d]|[,]|[\s])*\]$/ // this patterns grab whatever is inside [ ]
+    let reIndex:number;
+    let base_tag_index:number;
+    let base_tag:string = "";
+
+    let index:number | number[];
+    let indexStr:string;
+    // let indexArr:number[] = []
+
+    // get the array index
+    try {
+      reIndex = tag_name.search(array_pattern) // MyTag.Name[33,33,44] => 10 where 10 is the bracket [ index
+      indexStr = tag_name.substring(reIndex + 1,  tag_name.length -1) // 33,33,44
+      index = Number(indexStr)
+      if(indexStr.toString().includes(',')){
+        index = indexStr.toString().split(',').map(i => Number(i))
+      }
+    } catch (error) {
+      index = 0;
+    }
+
+    //  get the base tag name
+    base_tag_index = tag_name.search(bit_end_pattern)
+    if(base_tag_index != -1){
+      base_tag = tag_name.substring(0, base_tag_index)
+    }
+    base_tag_index = tag_name.search(array_pattern)
+    if(base_tag_index != -1){
+      base_tag = tag_name.substring(0, base_tag_index)
+    }
+    if(base_tag_index == -1){
+      base_tag = tag_name
+    }
+
+    return {tag_name, base_tag, index}
 }
--- src/lgx_response.ts
+++ src/lgx_response.ts
@@ -1,3 +1,64 @@
+import {Dictionary} from 'lodash'
+
 export class Response{
-    
+    private TagName;
+    private Value;
+    private Status;
+
+    // value can be anything but need to make it into an enum?
+    constructor(tag_name:string, value:any, status:number){
+        this.TagName = tag_name
+        this.Value = value
+        this.Status = cip_error_codes[status]
+    }
+
+    public toString(){
+        return `${this.TagName} ${this.Value} ${this.Status}`
+    }
 }
+
+const cip_error_codes:Dictionary<string>  = {0x00: 'Success',
+                   0x01: 'Connection failure',
+                   0x02: 'Resource unavailable',
+                   0x03: 'Invalid parameter value',
+                   0x04: 'Path segment error',
+                   0x05: 'Path destination unknown',
+                   0x06: 'Partial transfer',
+                   0x07: 'Connection lost',
+                   0x08: 'Service not supported',
+                   0x09: 'Invalid Attribute',
+                   0x0A: 'Attribute list error',
+                   0x0B: 'Already in requested mode/state',
+                   0x0C: 'Object state conflict',
+                   0x0D: 'Object already exists',
+                   0x0E: 'Attribute not settable',
+                   0x0F: 'Privilege violation',
+                   0x10: 'Device state conflict',
+                   0x11: 'Reply data too large',
+                   0x12: 'Fragmentation of a premitive value',
+                   0x13: 'Not enough data',
+                   0x14: 'Attribute not supported',
+                   0x15: 'Too much data',
+                   0x16: 'Object does not exist',
+                   0x17: 'Service fragmentation sequence not in progress',
+                   0x18: 'No stored attribute data',
+                   0x19: 'Store operation failure',
+                   0x1A: 'Routing failure, request packet too large',
+                   0x1B: 'Routing failure, response packet too large',
+                   0x1C: 'Missing attribute list entry data',
+                   0x1D: 'Invalid attribute value list',
+                   0x1E: 'Embedded service error',
+                   0x1F: 'Vendor specific',
+                   0x20: 'Invalid Parameter',
+                   0x21: 'Write once value or medium already written',
+                   0x22: 'Invalid reply received',
+                   0x23: 'Buffer overflow',
+                   0x24: 'Invalid message format',
+                   0x25: 'Key failure in path',
+                   0x26: 'Path size invalid',
+                   0x27: 'Unexpected attribute in list',
+                   0x28: 'Invalid member ID',
+                   0x29: 'Member not settable',
+                   0x2A: 'Group 2 only server general failure',
+                   0x2B: 'Unknown Modbus error',
+                   0x2C: 'Attribute not gettable'}
